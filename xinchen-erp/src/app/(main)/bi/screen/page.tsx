b19d4b17fbadef663e9b6ef108ca81937ded2410
zhaojie <927375718@qq.com>
2026-08-03 11:21:28 +0800

    修复: bi/screen 改为简单re-export,避免dynamic()SSR冲突

--- xinchen-erp/src/app/(main)/bi/screen/page.tsx
+++ xinchen-erp/src/app/(main)/bi/screen/page.tsx
@@ -1,11 +1,6 @@
 /**
  * BI 数据大屏
- * echarts 加载优化: 使用 next/dynamic 进一步优化可参考 ScreenClient.tsx
+ * echarts 通过 ScreenClient 独立 chunk 按需加载
  */
-import dynamic from "next/dynamic";
-
-const ScreenClient = dynamic(() => import("./ScreenClient"), { ssr: false });
-
-export default function BigScreenPage() {
-  return <ScreenClient />;
-}
+import ScreenClient from "./ScreenClient";
+export default ScreenClient;
